2d604f788c6bee2fee98d64f612d9da3d362e826
Madhav Hirani <madhavhirani2422@gmail.com>
2025-05-15 16:46:42 +0530

    image

--- src/components/aboutMeSection/AboutMePic.jsx
+++ src/components/aboutMeSection/AboutMePic.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const AboutMePic = () => {
   return (
     <div className='h-[500px] w-[300px] rounded-[2rem] overflow-hidden'>
       <img
-        src="portfolio/images/Portfolio-image.jpg"  // update the path as needed
+        src="/portfolio/images/Portfolio-image.jpg"  // ✅ fixed path for GitHub Pages
         alt="Portfolio"
         className='h-full w-full object-cover scale-105 hover:scale-110 transition-transform duration-700 ease-in-out'
       />
     </div>
-  )
-}
+  );
+};
 
-export default AboutMePic
+export default AboutMePic;
